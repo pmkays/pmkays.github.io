dcde3e36ecd0333d8479a969cd573ab60325c841
Paula Kurniawan <paula.kurn@gmail.com>
2020-11-13 13:26:28 +1100

    Refactored JS and directories + UI fixes

---
+++ scripts.js
@@ -0,0 +1,32 @@
+$(window).on('load', () => {
+  //remove the loading screen after site has finished loading
+  $(".loading-screen").fadeOut("slow");
+
+  $('#contact-form').submit((event) => {
+      event.preventDefault();
+      
+      //enable the loading spinner
+      $("#submit-btn").html('Sending &nbsp; <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true">');
+
+      emailjs.sendForm('contact_service', 'template_pujf54r', event.currentTarget, "user_VrEOLCVXUi06JSSqeXuqQ")
+          .then(() => {
+            //resets the form, button and displays resulting message to user
+              $('#contact-form').trigger('reset');
+              $("#submit-btn").html('Send');
+              $("#email-result").html("Message sent successfully! I'll get back to you as soon as possible.");
+          }, (error) => {
+              console.dir(error);
+              $("#email-result").html("Sorry, this message couldn't be sent. Please get in contact with me through email directly.");
+          });
+  });
+});
+
+$(window).scroll(() =>{scrollButtonHandler()});
+
+scrollButtonHandler = () => {
+  var scrollTop = $(window).scrollTop();
+  scrollTop > 100 ? $("#scroll-btn").show() : $("#scroll-btn").hide();
+}
+
+$("#scroll-btn").click(() => { $(window).scrollTop(0); });
+ 
