35a927d0c0826ee0cbaa1cf06eaa673e9deebad5
Paula Kurniawan <paula.kurn@gmail.com>
2020-11-14 09:37:35 +1100

    Add animations and lunardo image

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ $(window).on('load', () => {
   //remove the loading screen after site has finished loading
   $(".loading-screen").fadeOut("slow");
 
+  AOS.init();
+
   $('#contact-form').submit((event) => {
       event.preventDefault();
       
@@ -13,13 +15,36 @@ $(window).on('load', () => {
             //resets the form, button and displays resulting message to user
               $('#contact-form').trigger('reset');
               $("#submit-btn").html('Send');
-              $("#email-result").html("Message sent successfully! I'll get back to you as soon as possible.");
+              $("#email-result").hide().html("Message sent successfully! I'll get back to you as soon as possible.").fadeIn('slow');
           }, (error) => {
               console.dir(error);
-              $("#email-result").html("Sorry, this message couldn't be sent. Please get in contact with me through email directly.");
+              $("#email-result").hide().html("Sorry, this message couldn't be sent. Please get in contact with me through email directly.").fadeIn('slow');
           });
   });
 });
+
+
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.scroll-animations .animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeInLeft');
+      }
+    });
+  });
+});
+
 
 $(window).scroll(() =>{scrollButtonHandler()});
 
